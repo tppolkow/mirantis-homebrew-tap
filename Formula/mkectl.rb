4ae43398477a8606141ca2c37c20b34855afd986
Thomas Polkowski <tpolkowski@mirantis.com>
2025-01-24 16:36:00 -0500

    Fix sha

--- Formula/mkectl.rb
+++ Formula/mkectl.rb
@@ -4,15 +4,15 @@ class Mkectl < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/MirantisContainers/mke-release/releases/download/v4.0.1-rc.3/mkectl_darwin_x86_64.tar.gz"
-    sha256 "c31a6906d9da6dfaa02873c22bcc05a759a41a7902e3e734883121bd39faae49"
+    sha256 "54df9ee126f5c6e7bcf4d7bb8d98cf7fd2db11daa123d0c8e744997cecaa2959"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/MirantisContainers/mke-release/releases/download/v4.0.1-rc.3/mkectl_darwin_arm64.tar.gz"
-    sha256 "54df9ee126f5c6e7bcf4d7bb8d98cf7fd2db11daa123d0c8e744997cecaa2959"
+    sha256 "c31a6906d9da6dfaa02873c22bcc05a759a41a7902e3e734883121bd39faae49"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/MirantisContainers/mke-release/releases/download/v4.0.1-rc.3/mkectl_linux_x86_64.tar.gz"
     sha256 "8261544f42ab813d5ad7d3a0902be4e68dad9177411d0e7f28c6ad0277e699ba"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/MirantisContainers/mke-release/releases/download/v4.0.1-rc.3/mkectl_windows_arm64.zip"
+    url "https://github.com/MirantisContainers/mke-release/releases/download/v4.0.1-rc.3/mkectl_linux_arm64.zip"
     sha256 "6bf8927bf3e6e4c8e9435eaceec202651fdd7516d5dcab4ea67248f5f18e7a3e"
   else
     odie "Unsupported platform"
